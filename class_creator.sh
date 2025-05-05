f2f28532c8fee133bf40c5290fcd29eee631aed9
Borja Velasco <git@borjavelasco.me>
2025-05-05 20:33:23 +0200

    Improved class_creator.sh

--- class_creator.sh
+++ class_creator.sh
@@ -3,7 +3,7 @@
 NAME=$(echo $1 | tr '[:upper:]' '[:lower:]');
 CLASSFILE=$1.hpp
 
-if [ $2 ]; then
+if [[ $2 ]]; then
 	INHERENCE=": public $2"
 	I_INCLUDE="# include \"$2.hpp\""
 	fi
@@ -16,8 +16,14 @@ create_class_file()
 	cat ~/.local/bin/class_creator/blueprint.cpp | sed 's/$CLASSNAME/'$1'/g' | sed 's/$classname/'$2'/g' | sed 's/$CLASSFILE/'$CLASSFILE'/g' > $1.cpp
 }
 
-if [ $1 ]; then
+if [[ $1 ]]; then
+	if [[ -a "$1.cpp" || -a "$1.hpp" ]]; then
+		echo "Error: $1.cpp or $1.hpp already exists. Aborting to prevent overwrite"
+		exit 1
+	fi
 	create_class_file $1 $NAME
+	echo "The class file has been sucessfully created"
 else
-	echo "USAGE classcreator.sh classname [inherence]"
+	echo "ERROR: Argumtents are needed."
+	echo "USAGE: classcreator.sh classname [inherence]"
 fi
