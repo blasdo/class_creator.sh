a9187708e8d405509a14b48c8562281b868b7e34
Borja Velasco Aguado <git@borjavelasco.me>
2025-05-22 17:34:27 +0200

    added --no-debug flag

--- class_creator.sh
+++ class_creator.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-NAME=$(echo $1 | tr '[:upper:]' '[:lower:]');
-CLASSFILE=$1.hpp
+DebugMode=1
+
+while [[ $1 == -* ]]; do
+	if [[ $1 == "-n" ]] || [[ $1 == "--no-debug" ]]; then
+		DebugMode=0;
+	else
+		echo "Invalid args"
+		exit 1;
+	fi
+	shift 1;
+done
 
 if [[ $2 ]]; then
 	INHERENCE=": public $2"
 	I_INCLUDE="# include \"$2.hpp\""
 	fi
+
+NAME=$(echo $1 | tr '[:upper:]' '[:lower:]');
+CLASSFILE= $1.hpp
 
 create_class_file()
 {
@@ -16,12 +28,24 @@ create_class_file()
 	cat ~/.local/etc/class_creator/blueprint.cpp | sed 's/$CLASSNAME/'$1'/g' | sed 's/$classname/'$2'/g' | sed 's/$CLASSFILE/'$CLASSFILE'/g' > $1.cpp
 }
 
+create_class_file_nd()
+{
+	cat ~/.local/etc/class_creator/blueprint.hpp | sed 's/CLASSNAME_H/'"$1"'_H/g'\
+	| sed 's/$CLASSNAME/'$1'/g' | sed 's/$classname/'$2'/g'\
+		| sed 's/$INHERENCE/'"$INHERENCE"'/g' | sed 's/$I_INCLUDE/'"$I_INCLUDE"'/g'> $1.hpp
+	cat ~/.local/etc/class_creator/blueprint_nd.cpp | sed 's/$CLASSNAME/'$1'/g' | sed 's/$classname/'$2'/g' | sed 's/$CLASSFILE/'$CLASSFILE'/g' > $1.cpp
+}
+
 if [[ $1 ]]; then
 	if [[ -a "$1.cpp" || -a "$1.hpp" ]]; then
 		echo "Error: $1.cpp or $1.hpp already exists. Aborting to prevent overwrite"
 		exit 1
 	fi
-	create_class_file $1 $NAME
+	if [[ $DebugMode == 1 ]]; then
+		create_class_file $1 $NAME
+	elif [[ $DebugMode == 0 ]]; then
+		create_class_file_nd $1 $NAME
+	fi
 	echo "The class file has been sucessfully created"
 else
 	echo "ERROR: Argumtents are needed."
---
+++ utils/blueprint_nd.cpp
@@ -0,0 +1,18 @@
+#include "$CLASSFILE"
+#include <iostream>
+
+$CLASSNAME::$CLASSNAME()
+{
+}
+
+$CLASSNAME::$CLASSNAME($CLASSNAME const &$classname)
+{
+}
+
+$CLASSNAME::~$CLASSNAME()
+{
+}
+
+$CLASSNAME &$CLASSNAME::operator=($CLASSNAME const &$classname)
+{
+}
